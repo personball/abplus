380eaebc0bbbb4451631eb38fb7e02ff32c11c5d
personball <personball@163.com>
2016-12-13 14:15:06 +0800

    add abplus.web.api project

---
+++ src/Abplus.Web.Api/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using Abp;
+
+// 有关程序集的一般信息由以下
+// 控制。更改这些特性值可修改
+// 与程序集关联的信息。
+[assembly: AssemblyTitle("Abplus.Web.Api")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Abplus.Web.Api")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+//将 ComVisible 设置为 false 将使此程序集中的类型
+//对 COM 组件不可见。  如果需要从 COM 访问此程序集中的类型，
+//请将此类型的 ComVisible 特性设置为 true。
+[assembly: ComVisible(false)]
+
+// 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
+[assembly: Guid("84d80055-6a95-4de3-b1e7-68b88838f2be")]
+
+// 程序集的版本信息由下列四个值组成: 
+//
+//      主版本
+//      次版本
+//      生成号
+//      修订号
+//
+//可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
+// 方法是按如下所示使用“*”: :
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion(AbplusConsts.CurrentVersion)]
+[assembly: AssemblyFileVersion(AbplusConsts.CurrentVersion)]
---
+++ src/Abplus.Web.Api/WebApi/JsonMediaTypeFormatterExtension.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Http.Formatting;
+using Abp.Json;
+
+namespace Abp.WebApi
+{
+    public static class JsonMediaTypeFormatterExtension
+    {
+        public static void SetCustomDataFormatString(this JsonMediaTypeFormatter formatter, string dateTimeFormat)
+        {
+            var converters = formatter.SerializerSettings.Converters;
+            foreach (var converter in converters)
+            {
+                if (converter is AbpDateTimeConverter)
+                {
+                    var tmpConverter = converter as AbpDateTimeConverter;
+                    tmpConverter.DateTimeFormat = dateTimeFormat;
+                }
+            }
+        }
+    }
+}

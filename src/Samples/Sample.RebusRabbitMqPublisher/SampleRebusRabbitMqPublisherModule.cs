cd8ee6a259a672cfeb24b2a5d94e520f998d7c56
personball <personball@163.com>
2017-06-08 14:02:42 +0800

    sample config rebus use nlog

--- src/Samples/Sample.RebusRabbitMqPublisher/SampleRebusRabbitMqPublisherModule.cs
+++ src/Samples/Sample.RebusRabbitMqPublisher/SampleRebusRabbitMqPublisherModule.cs
@@ -4,6 +4,7 @@ using Abp.Modules;
 using Abp.MqMessages.Publishers;
 using Abp.Threading.BackgroundWorkers;
 using Castle.Facilities.Logging;
+using Rebus.NLog;
 using Sample.BackgroundWorks;
 
 namespace Sample
@@ -14,6 +15,7 @@ namespace Sample
         public override void PreInitialize()
         {
             Configuration.Modules.UseAbplusRebusRabbitMqPublisher()
+                .UseLogging(c => c.NLog())
                 .ConnectionTo("amqp://dev:dev@rabbitmq.local.jk724.cn/dev_host");
 
             Configuration.BackgroundJobs.IsJobExecutionEnabled = true;

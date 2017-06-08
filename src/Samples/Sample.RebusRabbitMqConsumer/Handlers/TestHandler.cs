b28121216b011a14efb4354bbfc24bfed1e9c57e
personball <personball@163.com>
2017-06-08 11:13:43 +0800

    add MqMessagesSamples

---
+++ src/Samples/Sample.RebusRabbitMqConsumer/Handlers/TestHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Castle.Core.Logging;
+using Rebus.Handlers;
+using Sample.MqMessages;
+
+namespace Sample.Handlers
+{
+    public class TestHandler : IHandleMessages<TestMqMessage>
+    {
+        public ILogger Logger { get; set; }
+
+        public TestHandler()
+        {
+            
+        }
+
+        public Task Handle(TestMqMessage message)
+        {
+            Logger.Debug($"{Logger.GetType()}:{message.Name},{message.Value},{message.Time}");
+
+            return Task.FromResult(0);
+        }
+    }
+}

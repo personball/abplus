330a179612ebaa0b87753fe5f18458b4c82bd17f
personball <personball@163.com>
2017-07-13 17:05:38 +0800

    排除旧组件的打包

--- Nupkg/pack.bat
+++ Nupkg/pack.bat
@@ -1,8 +1,5 @@
 NuGet "pack" "..\src\Abplus\Abplus.csproj" -Properties Configuration=Release
 NuGet "pack" "..\src\Abplus.Common\Abplus.Common.csproj" -Properties Configuration=Release
-NuGet "pack" "..\src\Abplus.Events.Producer\Abplus.Events.Producer.csproj" -Properties Configuration=Release
-NuGet "pack" "..\src\Abplus.RebusRabbitmqConsumer\Abplus.RebusRabbitmqConsumer.csproj" -Properties Configuration=Release
-NuGet "pack" "..\src\Abplus.RebusRabbitmqProducer\Abplus.RebusRabbitmqProducer.csproj" -Properties Configuration=Release
 NuGet "pack" "..\src\Abplus.Web.Api\Abplus.Web.Api.csproj" -Properties Configuration=Release
 NuGet "pack" "..\src\Abplus.Web.SignalR\Abplus.Web.SignalR.csproj" -Properties Configuration=Release
 NuGet "pack" "..\src\Abplus.Web.SimpleCaptcha\Abplus.Web.SimpleCaptcha.csproj" -Properties Configuration=Release

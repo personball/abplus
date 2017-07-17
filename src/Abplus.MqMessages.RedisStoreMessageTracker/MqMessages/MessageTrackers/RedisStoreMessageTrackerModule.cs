0e5cab45f38d54585f9666fd725ff6072cdeea73
personball <personball@163.com>
2017-07-17 18:29:45 +0800

    add proj Abplus.MqMessages.RedisStoreMessageTracker

---
+++ src/Abplus.MqMessages.RedisStoreMessageTracker/MqMessages/MessageTrackers/RedisStoreMessageTrackerModule.cs
@@ -0,0 +1,20 @@
+﻿using Abp.Modules;
+using Abp.Runtime.Caching.Redis;
+
+namespace Abp.MqMessages.MessageTrackers
+{
+    [DependsOn(typeof(AbpRedisCacheModule))]
+    public class RedisStoreMessageTrackerModule : AbpModule
+    {
+        public override void PreInitialize()
+        {
+            IocManager.Register<IMessageTracker, RedisStoreMessageTracker>();
+        }
+
+        public override void Initialize()
+        {
+            //IocManager.RegisterIfNot<ISerializer, NewtonsoftSerializer>(DependencyLifeStyle.Singleton);
+            //IocManager.RegisterIfNot<ICacheClient, StackExchangeRedisCacheClient>(DependencyLifeStyle.Singleton);
+        }
+    }
+}

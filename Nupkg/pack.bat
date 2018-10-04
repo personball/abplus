3901466e44d24ea80eaae21a5529ae1ee58cdc15
personball <personball@163.com>
2018-10-04 21:58:02 +0800

    clean

--- Nupkg/pack.bat
+++ Nupkg/pack.bat
@@ -1,35 +1,9 @@
-NuGet "pack" "..\src\Abplus\Abplus.csproj" -Properties Configuration=Release
-
-
-NuGet "pack" "..\src\Abplus.Web.PlugIns\Abplus.Web.PlugIns.csproj" -Properties Configuration=Release 
-
-NuGet "pack" "..\src\Abplus.Common\Abplus.Common.csproj" -Properties Configuration=Release
-
-NuGet "pack" "..\src\Abplus.Web.Api\Abplus.Web.Api.csproj" -Properties Configuration=Release
-
-NuGet "pack" "..\src\Abplus.Web.SignalR\Abplus.Web.SignalR.csproj" -Properties Configuration=Release
-
-NuGet "pack" "..\src\Abplus.Web.SimpleCaptcha\Abplus.Web.SimpleCaptcha.csproj" -Properties Configuration=Release
-
-NuGet "pack" "..\src\Abplus.WebApiClient\Abplus.WebApiClient.csproj" -Properties Configuration=Release
-
-NuGet "pack" "..\src\Abplus.MqMessages\Abplus.MqMessages.csproj" -Properties Configuration=Release
-
-NuGet "pack" "..\src\Abplus.MqMessages.RebusConsumer\Abplus.MqMessages.RebusRabbitMqConsumer.csproj" -Properties Configuration=Release
-
-NuGet "pack" "..\src\Abplus.MqMessages.RebusPublisher\Abplus.MqMessages.RebusRabbitMqPublisher.csproj" -Properties Configuration=Release
-
-NuGet "pack" "..\src\Abplus.EntityFramework\Abplus.EntityFramework.csproj" -Properties Configuration=Release
-
-NuGet "pack" "..\src\Abplus.MqMessages.RebusCore\Abplus.MqMessages.RebusCore.csproj" -Properties Configuration=Release
-
-NuGet "pack" "..\src\Abplus.MqMessages.AuditingStore\Abplus.MqMessages.AuditingStore.csproj" -Properties Configuration=Release
-
-NuGet "pack" "..\src\Abplus.MqMessages.RedisStoreMessageTracker\Abplus.MqMessages.RedisStoreMessageTracker.csproj" -Properties Configuration=Release
-
-NuGet "pack" "..\src\Abplus.T4.PermissionsFromJson\Abplus.T4.PermissionsFromJson.csproj" -Properties Configuration=Release
-
-NuGet "pack" "..\src\Abplus.MqMessages.IndexToES\Abplus.MqMessages.IndexToES.csproj" -Properties Configuration=Release
-
-
-pause
+dotnet pack ..\src\Abplus\Abplus.csproj -o ..\..\nupkg\
+dotnet pack ..\src\Abplus.MqMessages\Abplus.MqMessages.csproj -o ..\..\nupkg\
+dotnet pack ..\src\Abplus.MqMessages.AuditingStore\Abplus.MqMessages.AuditingStore.csproj -o ..\..\nupkg\
+dotnet pack ..\src\Abplus.MqMessages.IndexToES\Abplus.MqMessages.IndexToES.csproj -o ..\..\nupkg\
+dotnet pack ..\src\Abplus.MqMessages.RebusCore\Abplus.MqMessages.RebusCore.csproj -o ..\..\nupkg\
+dotnet pack ..\src\Abplus.MqMessages.RebusRabbitMqConsumer\Abplus.MqMessages.RebusRabbitMqConsumer.csproj -o ..\..\nupkg\
+dotnet pack ..\src\Abplus.MqMessages.RebusRabbitMqPublisher\Abplus.MqMessages.RebusRabbitMqPublisher.csproj -o ..\..\nupkg\
+dotnet pack ..\src\Abplus.MqMessages.RedisStoreMessageTracker\Abplus.MqMessages.RedisStoreMessageTracker.csproj -o ..\..\nupkg\
+dotnet pack ..\src\Abplus.T4.PermissionsFromJson\Abplus.T4.PermissionsFromJson.csproj -o ..\..\nupkg\

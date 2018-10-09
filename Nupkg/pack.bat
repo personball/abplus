63df8885971f2567ff02c857ac549041af429ac9
personball <personball@163.com>
2018-10-09 23:13:57 +0800

    升级版本，发布2.0.1nuget包

--- Nupkg/pack.bat
+++ Nupkg/pack.bat
@@ -1,6 +1,7 @@
 dotnet pack ..\src\Abplus\Abplus.csproj -o ..\..\nupkg\
 dotnet pack ..\src\Abplus.AspNetCore.SignalR\Abplus.AspNetCore.SignalR.csproj -o ..\..\nupkg\
 dotnet pack ..\src\Abplus.MqMessages\Abplus.MqMessages.csproj -o ..\..\nupkg\
+dotnet pack ..\src\Abplus.MqMessages.AuditingConsumerHandler\Abplus.MqMessages.AuditingConsumerHandler.csproj -o ..\..\nupkg\
 dotnet pack ..\src\Abplus.MqMessages.AuditingStore\Abplus.MqMessages.AuditingStore.csproj -o ..\..\nupkg\
 dotnet pack ..\src\Abplus.MqMessages.IndexToES\Abplus.MqMessages.IndexToES.csproj -o ..\..\nupkg\
 dotnet pack ..\src\Abplus.MqMessages.RebusCore\Abplus.MqMessages.RebusCore.csproj -o ..\..\nupkg\

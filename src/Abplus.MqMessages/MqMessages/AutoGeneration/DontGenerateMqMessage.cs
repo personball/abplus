cf1eec1162b98ea63648a5c71a5f5dc430d5e24c
personball <personball@163.com>
2018-11-10 09:15:52 +0800

    upgrade abp to v4.0.1

---
+++ src/Abplus.MqMessages/MqMessages/AutoGeneration/DontGenerateMqMessage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Abp.MqMessages.AutoGeneration
+{
+    /// <summary>
+    /// 标记指定EventData不参与自动生成MqMessage
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, Inherited = false)]
+    public class DontGenerateMqMessage : Attribute
+    {
+    }
+}
